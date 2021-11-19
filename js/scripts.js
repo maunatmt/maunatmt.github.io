2cbddb0baaec31da736ecf59a47185e3c955c708
maunatmt <pop.orange.1@gmail.com>
2021-11-19 12:35:43 +0200

    Update json

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,25 @@
- $("#navTgl").on("click", function(){
-    $("#wrap").toggleClass('dark-color');
-});
+//  $("#navTgl").on("click", function(){
+//     $("#wrap").toggleClass('dark-color');
+// });
 new Vue({
-  el: '#figures',
+  el: '#wrap',
   data: {
       media: [],
+      sales: [],
+      labels: [],
+      datasets:[],
+      salesLabels: [],
+      salesDatasets:[],
+      rankingLabels: [],
+      rankingDatasets:[],
+      groupingLabels: [],
+      groupingDatasets:[],
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
       }
-  },
-  mounted: function() {
-      var self = this;
-      axios
-          .get('https://raw.githubusercontent.com/maunatmt/maunatmt.github.io/main/misc/data.json', self.headers)
-          .then(function(response) {
-              self.media = response.data.media;
-          })
-          .catch(function(error) {
-              console.log('Failed to load.', error);
-          })
-  }
-})
-
-new Vue({
-  el: '#chart',
-  data: {
-    labels: [],
-    datasets:[],
-    rankingLabels: [],
-    rankingDatasets:[],
-    groupingLabels: [],
-    groupingDatasets:[],
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-      "Access-Control-Allow-Origin": "*",
-    }
   },
   methods:{
     displayTrend: function(){
@@ -53,9 +35,29 @@ new Vue({
           maintainAspectRatio: false,
           scales: {  
             y: {
-                  beginAtZero: true,
-                  stepSize: 100
-              }
+              beginAtZero: true,
+              stepSize: 100
+            }
+          }
+        }
+      });
+    },
+    displaySalesTrend: function(){
+      var ctx = document.getElementById('sales-trend-chart').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: this.salesLabels,
+            datasets: this.salesDatasets
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {  
+            y: {
+              beginAtZero: true,
+              stepSize: 100
+            }
           }
         }
       });
@@ -131,15 +133,138 @@ new Vue({
   },
   mounted: function(){
     axios.get('https://raw.githubusercontent.com/maunatmt/maunatmt.github.io/main/misc/data.json', self.headers).then(response =>{
-      this.datasets = response.data.trend.datasets;
+      this.media = response.data.media;
+      this.sales = response.data.sales;
       this.labels = response.data.trend.labels;
+      this.datasets = response.data.trend.datasets;
+      this.salesLabels = response.data.salesTrend.labels;
+      this.salesDatasets = response.data.salesTrend.datasets;
       this.rankingLabels = response.data.ranking.labels;
       this.rankingDatasets = response.data.ranking.datasets;
       this.groupingLabels = response.data.grouping.labels;
       this.groupingDatasets = response.data.grouping.datasets;
       this.displayTrend();
+      this.displaySalesTrend();
       this.displayRanking();
       this.displayGrouping();
     })
   }
 });
+// new Vue({
+//   el: '#chart',
+//   data: {
+//     labels: [],
+//     datasets:[],
+//     rankingLabels: [],
+//     rankingDatasets:[],
+//     groupingLabels: [],
+//     groupingDatasets:[],
+//     headers: {
+//       'Content-Type': 'application/json;charset=UTF-8',
+//       "Access-Control-Allow-Origin": "*",
+//     }
+//   },
+//   methods:{
+//     displayTrend: function(){
+//       var ctx = document.getElementById('trend-chart').getContext('2d');
+//       var myChart = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//             labels: this.labels,
+//             datasets: this.datasets
+//         },
+//         options: {
+//           responsive: true,
+//           maintainAspectRatio: false,
+//           scales: {  
+//             y: {
+//                   beginAtZero: true,
+//                   stepSize: 100
+//               }
+//           }
+//         }
+//       });
+//     },
+//     displayRanking: function(){
+//       var ctx = document.getElementById('ranking-chart').getContext('2d');
+//       var myChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: this.rankingLabels,
+//             datasets: this.rankingDatasets
+//         },
+//         options: {
+//           indexAxis: 'y',
+//           responsive: true,
+//           maintainAspectRatio: false,
+//           scales: {  
+//             x: {
+//               ticks: {
+//                 stepSize: 20,
+//                 callback: function(value, index, values){
+//                   return  value +  '%'
+//                 }
+//               }
+//             }
+//           },
+//           plugins: {
+//             legend : {
+//                      display : false
+//               }
+//           }
+//         }
+//       });
+//     },
+//     displayGrouping: function(){
+//       var ctx = document.getElementById('grouping-chart').getContext('2d');
+//       var myChart = new Chart(ctx, {
+//         type: 'scatter',
+//         data: {
+//             labels: this.groupingLabels,
+//             datasets: this.groupingDatasets
+//         },
+//         options: {
+//           responsive: true,
+//           maintainAspectRatio: false,
+//           scales: {
+//             x: {
+//               title: {
+//                   display: true,
+//                   text: 'Age',
+//               },
+//               ticks: {
+//                   stepSize: 10
+//               },
+//             },
+//             y: {
+//               title: {
+//                 display: true,
+//                 text: 'Stay Time',
+//               },
+//               beginAtZero: true,
+//               ticks: {
+//                 stepSize: 10,
+//                 callback: function(value, index, values){
+//                   return  value +  'min'
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       });
+//     }
+//   },
+//   mounted: function(){
+//     axios.get('https://raw.githubusercontent.com/maunatmt/maunatmt.github.io/main/misc/data.json', self.headers).then(response =>{
+//       this.datasets = response.data.trend.datasets;
+//       this.labels = response.data.trend.labels;
+//       this.rankingLabels = response.data.ranking.labels;
+//       this.rankingDatasets = response.data.ranking.datasets;
+//       this.groupingLabels = response.data.grouping.labels;
+//       this.groupingDatasets = response.data.grouping.datasets;
+//       this.displayTrend();
+//       this.displayRanking();
+//       this.displayGrouping();
+//     })
+//   }
+// });
